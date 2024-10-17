5c56f7d7bbb9012dd930d217da4c29cd8207a7be
ManuRodriguezC <estebanclimb@gmail.com>
2024-10-17 18:13:24 -0500

    Update errors and warnings

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,40 +1,40 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: import.meta.env.GMAIL_USER,
-        pass: import.meta.env.GAMIL_PASS,
-    },
-})
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: import.meta.env.GMAIL_USER,
+//         pass: import.meta.env.GAMIL_PASS,
+//     },
+// })
 
-export const POST = async ({request}) => {
-    const data = await request.json()
-    console.log(data)
-    const { name, phone, address, info } = data
+// export const POST = async ({request}) => {
+//     const data = await request.json()
+//     console.log(data)
+//     const { name, phone, address, info } = data
 
-    try {
-        const mailOptions = {
-            from: import.meta.env.GMAIL_USER,
-            to: 'manu.rodriguezc.dev@gmail.com',
-            subject: 'Solicitud de Cotización',
-            text: data
-        }
-        await transporter.sendMail(mailOptions)
+//     try {
+//         const mailOptions = {
+//             from: import.meta.env.GMAIL_USER,
+//             to: 'manu.rodriguezc.dev@gmail.com',
+//             subject: 'Solicitud de Cotización',
+//             text: data
+//         }
+//         await transporter.sendMail(mailOptions)
 
-        return new Response(JSON.stringify({ success: true, message: 'Email sent successfull'}), {
-            status: 200,
-            headers: {
-                'Content-type': 'application/json'
-            },
-        });
-    } catch (error) {
-        console.error('Error sending email:', error)
-        return new Response(JSON.stringify({ success: false, message: 'Failed to send email'}), {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
-}
+//         return new Response(JSON.stringify({ success: true, message: 'Email sent successfull'}), {
+//             status: 200,
+//             headers: {
+//                 'Content-type': 'application/json'
+//             },
+//         });
+//     } catch (error) {
+//         console.error('Error sending email:', error)
+//         return new Response(JSON.stringify({ success: false, message: 'Failed to send email'}), {
+//             status: 500,
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//     }
+// }
