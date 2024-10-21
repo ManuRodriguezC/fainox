5a681147e8dd86bbce4a264cb07994af0f53b2b5
ManuRodriguezC <estebanclimb@gmail.com>
2024-10-21 08:57:38 -0500

    Add list page products and product page

--- src/store/productos.ts
+++ src/store/productos.ts
@@ -1,149 +1,149 @@
 export const productos = [
     {
         "name": "Tanque de 1400 Litros",
-        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos"],
+        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos", "Componentes"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "tanques",
-        "images": ["/products/producto1.jpg"],
+        "images": ["/products/17.webp"],
     },
     {
         "name": "Triturador de Alimentos",
-        "industry": ["Alimentos Secos"],
+        "industry": ["Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto2.jpg"]
+        "images": ["/products/15.webp"]
     },
     {
         "name": "Despulpador",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Farmaceuticos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto3.jpg"]
+        "images": ["/products/8.webp"]
     },
     {
         "name": "Combinador Aluminio 800SD",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "industry": ["Farmaceuticos", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "lineas UHT",
-        "images": ["/products/producto4.jpg"]
+        "images": ["/products/7.webp"]
     },
     {
         "name": "Pulverizador 5000-G8",
-        "industry": ["Alimentos Secos"],
+        "industry": ["Alimentos Varios", "Soluciones para Lavado", "Lacteos y Derivados"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto5.jpg"]
+        "images": ["/products/34-485.webp"]
     },
     {
         "name": "Descremador 2500l",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "molinos colidales",
-        "images": ["/products/producto6.jpg"]
+        "images": ["/products/18.webp"]
     },
     {
         "name": "Sistema de Mantequilla",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "industry": ["Conservas, Salsas y Aderezos", "Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto7.jpg"]
+        "images": ["/products/7.webp"]
     },
     {
-        "name": "Tanque de 1400 Litros",
-        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos"],
+        "name": "Tanque de 1400 Litros 2",
+        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos", "Componentes"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "formadoras de queso",
-        "images": ["/products/producto1.jpg"]
+        "images": ["/products/8.webp"]
     },
     {
-        "name": "Triturador de Alimentos",
-        "industry": ["Alimentos Secos"],
+        "name": "Triturador de Alimentos 1",
+        "industry": ["Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "pulverizadores",
-        "images": ["/products/producto2.jpg"]
+        "images": ["/products/15.webp"]
     },
     {
-        "name": "Despulpador",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "name": "Despulpador 1",
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto3.jpg"]
+        "images": ["/products/17.webp"]
     },
     {
-        "name": "Combinador Aluminio 800SD",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "name": "Combinador Aluminio 800SD 1",
+        "industry": ["Conservas, Salsas y Aderezos", "Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "frezzers",
-        "images": ["/products/producto4.jpg"]
+        "images": ["/products/18.webp"]
     },
     {
-        "name": "Pulverizador 5000-G8",
-        "industry": ["Alimentos Secos"],
+        "name": "Pulverizador 5000-G8 1",
+        "industry": ["Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "agitadores",
-        "images": ["/products/producto5.jpg"]
+        "images": ["/products/34-485.webp"]
     },
     {
-        "name": "Descremador 2500l",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "name": "Descremador 2500l 1",
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "autoclaves",
-        "images": ["/products/producto6.jpg"]
+        "images": ["/products/7.webp"]
     },
     {
-        "name": "Sistema de Mantequilla",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "name": "Sistema de Mantequilla 1",
+        "industry": ["Conservas, Salsas y Aderezos", "Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "envasadoras",
-        "images": ["/products/producto7.jpg"]
+        "images": ["/products/8.webp"]
     },
     {
-        "name": "Tanque de 1400 Litros",
-        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos"],
+        "name": "Tanque de 1400 Litros 2",
+        "industry": ["Lacteos y Derivados", "Frutas, Pulpa y Jugos", "Componentes"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "empacadoras",
-        "images": ["/products/producto1.jpg"]
+        "images": ["/products/15.webp"]
     },
     {
-        "name": "Triturador de Alimentos",
-        "industry": ["Alimentos Secos"],
+        "name": "Triturador de Alimentos 2",
+        "industry": ["Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "descremadoras",
-        "images": ["/products/producto2.jpg"]
+        "images": ["/products/17.webp"]
     },
     {
-        "name": "Despulpador",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "name": "Despulpador 2",
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "homogenizadores",
-        "images": ["/products/producto3.jpg"]
+        "images": ["/products/18.webp"]
     },
     {
-        "name": "Combinador Aluminio 800SD",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "name": "Combinador Aluminio 800SD 2",
+        "industry": ["Conservas, Salsas y Aderezos", "Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "tinas para queso",
-        "images": ["/products/producto4.jpg"]
+        "images": ["/products/34-485.webp"]
     },
     {
-        "name": "Pulverizador 5000-G8",
-        "industry": ["Alimentos Secos"],
+        "name": "Pulverizador 5000-G8 2",
+        "industry": ["Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "silos para leche",
-        "images": ["/products/producto5.jpg"]
+        "images": ["/products/7.webp"]
     },
     {
-        "name": "Descremador 2500l",
-        "industry": ["Frutas, Pulpa y Jugos", "Farmaceuticos"],
+        "name": "Descremador 2500l 2",
+        "industry": ["Frutas, Pulpa y Jugos", "Componentes", "Conservas, Salsas y Aderezos"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "sistemas de bombeo",
-        "images": ["/products/producto6.jpg"]
+        "images": ["/products/8.webp"]
     },
     {
-        "name": "Sistema de Mantequilla",
-        "industry": ["Farmaceuticos", "Alimentos Secos"],
+        "name": "Sistema de Mantequilla 2",
+        "industry": ["Conservas, Salsas y Aderezos", "Alimentos Varios", "Soluciones para Lavado"],
         "description": "Este es un tanque para que puedas transportar tu leche a todos los rincones del mundo",
         "linea": "soluciones para lavado",
-        "images": ["/products/producto7.jpg"]
+        "images": ["/products/15.webp"]
     }
 ]
