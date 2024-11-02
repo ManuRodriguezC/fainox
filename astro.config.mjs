9cfc5453d221889180373778e67b215504da0d34
ManuRodriguezC <estebanclimb@gmail.com>
2024-11-02 10:29:13 -0500

    check with vercel and hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-// import vercel from '@astrojs/vercel/serverless'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  output: 'hybrid',
+  adapter: vercel({
+    isr: true
   })
 });
