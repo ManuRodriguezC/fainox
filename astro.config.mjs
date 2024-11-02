bd1f9633ccfe627650dc2a922d4df0dc4487b8e2
ManuRodriguezC <estebanclimb@gmail.com>
2024-11-02 10:34:04 -0500

    check witt hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  adapter: vercel({
-    isr: true
-  })
 });
