8883bcc3a8208fa18e5f10f910fe8fb6f709e40d
ManuRodriguezC <estebanclimb@gmail.com>
2024-11-02 19:27:11 -0500

    test of deploy with server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel()
 });
