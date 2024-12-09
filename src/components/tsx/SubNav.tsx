e85c4e5e34cb09d0b13ccd2580925c45589f5d1f
ManuRodriguezC <estebanclimb@gmail.com>
2024-12-09 10:41:47 -0500

    Update sub nav and remove text in banner

--- src/components/tsx/SubNav.tsx
+++ src/components/tsx/SubNav.tsx
@@ -14,16 +14,17 @@ export default function SubNav({ indus, setFilter, filter }: Props) {
             <ul className="flex flex-row bg-colorBlue rounded-none md:rounded-lg pt-2 justify-center items-center md:gap-6">
                 <li className="flex justify-center items-center">
                     <button
-                        className={`text-xs md:text-base xl:text-xl py-4 px-1 transition-all duration-150 ${filter === "" ? "bg-white text-black rounded-higt" : "text-white font-semibold"}`}
+                        className={`text-xs md:text-base xl:text-xl py-2 px-1 transition-all duration-150 ${filter === "" ? "bg-white text-black rounded-md" : "text-white font-semibold"}`}
                         onClick={() => setFilter("")}>Todos</button>
                 </li>
                 {
                     subIndustrys[indus].map((sub: DatesIndustrys) => {
+                        const size = subIndustrys[indus].length == 5 ? 0 : 4
                         return (
                             <li>
                                 <button
-                                    className={`text-sss md:text-base xl:text-xl py-3 md:p-4 transition-all duration-150
-                                        ${filter === sub.name ? "bg-white text-black rounded-higt" : "text-white font-semibold hover:backdrop-contrast-75 rounded-higt"}`}
+                                    className={`text-sss md:text-base xl:text-xl py-3 md:p-4 transition-all duration-150 mx-${size} px-2
+                                        ${filter === sub.name ? "bg-white text-black rounded-md" : "text-white font-semibold hover:backdrop-contrast-75 rounded-md"}`}
                                     onClick={() => setFilter(sub.name)}>
                                     {capitalize(sub.name)}
                                 </button>
--- src/store/industrias.ts
+++ src/store/industrias.ts
@@ -25,7 +25,7 @@ export const sectores = [
   {
     title: "Lacteos y Derivados",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     link: "/industriasyservicios/Lacteos y Derivados",
     image: "/industrys/milk.webp",
     backgraund: "bg-custom-gradient",
@@ -45,7 +45,7 @@ export const sectores = [
   {
     title: "Frutas, Pulpa y Jugos",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     link: "/industriasyservicios/Frutas, Pulpa y Jugos",
     image: "/industrys/fruts.jpg",
     backgraund: "bg-custom-blue-gradient",
@@ -59,7 +59,7 @@ export const sectores = [
   {
     title: "Conservas, Salsas y Aderezos",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     link: "/industriasyservicios/Conservas, Salsas y Aderezos",
     image: "/industrys/salsas.webp",
     backgraund: "bg-custom-blue2-gradient",
@@ -73,7 +73,7 @@ export const sectores = [
   {
     title: "Alimentos Varios",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     link: "/industriasyservicios/Alimentos Varios",
     image: "/industrys/aceites.webp",
     backgraund: "bg-custom-gradient",
